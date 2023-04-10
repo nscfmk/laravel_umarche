6ced41e56afd0b3d560f2723663188aaa0e63839
nscfmk <mancity0710icloud.com>
2023-04-10 16:32:44 +0900

    属性で設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,9 @@
 <x-tests.app>
- <x-slot name="header">
-    ヘッダー１
- </x-slot>
-コンポーネントテスト１
+   <x-slot name="header">
+      ヘッダー１
+   </x-slot>
+   コンポーネントテスト１
+
+   <x-tests.card title="タイトル" content="本文"/>
+   
 </x-tests.app>
