b5c3c99deae3ee6ad22d8426e34ea42d20165d0d
nscfmk <mancity0710icloud.com>
2023-04-18 19:45:15 +0900

    カスタムリクエストの作成

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -4,11 +4,14 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            <x-input-error :messages="$errors->get('image')" class="mt-2" />
                    <form method="post" action="{{ route('owner.shops.update', ['shop' => $shop->id] )}}" enctype="multipart/form-data" >
                     @csrf
                     <div class="-m-2">
@@ -16,7 +19,7 @@
                             <div class="relative">
                                 <label for="image"
                                     class="leading-7 text-sm text-gray-600">画像</label>
-                                <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg"
+                                <input type="file" id="image" name="image"
                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                         </div>
