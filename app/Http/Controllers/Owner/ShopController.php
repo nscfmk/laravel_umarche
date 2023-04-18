32df643ba8fdf6c8e89c1b5b0ab35db4a9a15ddb
nscfmk <mancity0710icloud.com>
2023-04-18 12:05:52 +0900

    画像アップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,7 +7,8 @@ use App\Models\Owner;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
 
 class ShopController extends Controller
 {
@@ -38,25 +39,17 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        $owner = Owner::findOrFail($id);
-        return view('admin.owners.edit', compact('owner'));
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
-        $owner = Owner::findOrFail($id);
-        $owner->name = $request->name;
-        $owner->email = $request->email;
-        $owner->password = Hash::make($request->password);
+        $imageFile = $request->image;
+        if(!is_null($imageFile) && $imageFile->isValid()){
+            Storage::putFile('public/shops', $imageFile);
+        }
 
-        $owner->save();
-
-        return redirect()->route('admin.owners.index')
-            ->with([
-                'message' => 'オーナー情報を更新しました！！！',
-                'status' => 'info',
-            ]);
+        return redirect()->route('owner.shops.index');
     }
-
-    
 }
