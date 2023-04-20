4c6bb07f8dce210a369643d0efb99c2feb47b801
nscfmk <mancity0710icloud.com>
2023-04-20 17:19:55 +0900

    image seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
