67aa06f271197b3096db746ff72db05fb97e6850
nscfmk <mancity0710icloud.com>
2023-04-24 10:19:35 +0900

    stock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
+            StockSeeder::class,
         ]);
     }
 }
