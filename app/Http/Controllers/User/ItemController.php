362bc06aa9f992c7cb844ae8a4a0114cd9616679
nscfmk <mancity0710icloud.com>
2023-04-29 18:35:49 +0900

    商品の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Models\Stock;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -53,6 +54,11 @@ class ItemController extends Controller
 
     public function show($id){
         $product = Product::findOrFail($id);
-        return view('user.show' , compact('product'));
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+        if($quantity > 9){
+            $quantity = 9;
+        }
+        return view('user.show' , compact('product', 'quantity'));
     }
 }
