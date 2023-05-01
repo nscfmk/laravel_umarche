cce999ce6b5543193ddf877ba404fec967b37de3
nscfmk <mancity0710icloud.com>
2023-05-01 16:30:03 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Cart;
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -50,4 +50,52 @@ class CartController extends Controller
 
     return redirect()->route('user.cart.index');
   }
+  
+  public function checkout(){
+    $user = User::findOrFail(Auth::id());
+    $products = $user->products;
+ 
+    $lineItems = [];
+    foreach ($products as $product) {
+        $quantity = '';
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+        if ($product->pivot->quantity > $quantity) {
+            return redirect()->route('user.cart.index');
+        } else {
+            $lineItem = [
+                'name' => $product->name,
+                'description' => $product->information,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,
+            ];
+            array_push($lineItems, $lineItem);
+        }
+    }
+
+    foreach ($products as $product) {
+        Stock::create([
+            'product_id' => $product->id,
+            'type' => \Constant::PRODUCT_LIST['reduce'],
+            'quantity' => $product->pivot->quantity * -1
+        ]);
+    }
+
+    \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+    $session = \Stripe\Checkout\Session::create([
+        'payment_method_types' => ['card'],
+        'line_items' => [$lineItems],
+        'mode' => 'payment',
+        'success_url' => route('user.items.index'),
+        'cancel_url' => route('user.cart.index'),
+    ]);
+ 
+    $publicKey = env('STRIPE_PUBLIC_KEY');
+
+    return view(
+        'user.checkout',
+        compact('session', 'publicKey')
+    );
 }
