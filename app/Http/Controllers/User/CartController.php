4929d7dc35fdc469ec99801d6e81e42f0f729934
nscfmk <mancity0710icloud.com>
2023-05-02 10:37:33 +0900

    決済後のカート内商品削除

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -96,7 +96,7 @@ class CartController extends Controller
         'payment_method_types' => ['card'],
         'line_items' => [$lineItems],
         'mode' => 'payment',
-        'success_url' => route('user.items.index'),
+        'success_url' => route('user.cart.success'),
         'cancel_url' => route('user.cart.index'),
     ]);
 
@@ -107,4 +107,10 @@ class CartController extends Controller
         compact('session', 'publicKey')
     );
  }
+
+  public function success()
+  {
+    Cart::where('user_id', Auth::id())->delete();
+    return redirect()->route('user.items.index');
+  }
 }
